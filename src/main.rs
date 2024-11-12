2003b33dfc98d608e19df24eda159f4c5bbab161
Markus <fritzsma@gmail.com>
2024-11-12 21:48:40 +0100

    Add mutex parsing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use nom::{
     bytes::complete::tag,
     character::complete::{digit1, line_ending, alphanumeric1, i32, u32, not_line_ending},
     combinator::map_res,
-    sequence::{delimited, preceded},
+    sequence::{delimited, preceded, separated_pair},
     IResult,
 };
 use std::fs;
@@ -64,6 +64,7 @@ fn parse_variable(input: &str) -> IResult<&str, ExplicitVariable> {
         input = loop_input;
     }
     let (input, _) = tag("end_variable")(input)?;
+    let (input, _) = line_ending(input)?;
     let var = ExplicitVariable {
         domain_size,
         name: variable_name.to_string(),
@@ -75,20 +76,80 @@ fn parse_variable(input: &str) -> IResult<&str, ExplicitVariable> {
 }
 
 fn parse_all_variables(input: &str) -> IResult<&str, Vec<ExplicitVariable>> {
+    let (input, num_variables) = u32(input)?;
+    let (input, _) = line_ending(input)?;
+    println!("Number of variables: {}", num_variables);
     let mut variables = Vec::new();
     let mut input = input;
-    loop {
-        let variable_result = parse_variable(input);
-        match variable_result {
-            Ok((loop_input, var)) => {
-                variables.push(var);
-                input = loop_input;
-            }
-            Err(_) => break,
-            
-        }
+    for _ in 0..num_variables {
+        let (loop_input, var) = parse_variable(input)?;
+        variables.push(var);
+        input = loop_input;
     }
     Ok((input, variables))
+}
+
+fn parse_integer(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, str::parse::<u32>)(input)
+}
+
+fn parse_mutex_group(input: &str) -> IResult<&str, Vec<(u32, u32)>> {
+    let (input, _) = tag("begin_mutex_group")(input)?;
+    let (input, _) = line_ending(input)?;
+    
+    let (input, num_facts) = u32(input)?; 
+    let (input, _) = line_ending(input)?;
+    let mut input = input;
+
+    let mut mutex_group = Vec::with_capacity(num_facts as usize);
+    for _ in 0..num_facts {
+        let mut parser = separated_pair(parse_integer, tag(" "), parse_integer);
+        let (new_input, fact) = parser(input)?;
+        mutex_group.push(fact);
+        let (new_input, _) = line_ending(new_input)?;
+        input = new_input;
+    }
+
+    let (input, _) = tag("end_mutex_group")(input)?;
+    let (input, _) = line_ending(input)?;
+
+    Ok((input, mutex_group))
+}
+
+
+
+fn parse_mutexes(input: &str) -> IResult<&str, Vec<Vec<String>>> {
+    let (input, num_mutexes) = u32(input)?;
+    let (input, _) = line_ending(input)?;
+    let mut input = input;
+
+    let mut mutexes = Vec::with_capacity(num_mutexes as usize);
+
+    for _ in 0..num_mutexes {
+        let (new_input, mutex_group) = parse_mutex_group(input)?;
+        mutexes.push(mutex_group);
+        input = new_input;
+    }
+   Ok((input, vec![]))
+}
+
+
+fn parse_state(input: &str) -> IResult<&str, Vec<i32>> {
+    let (input, _) = tag("begin_state")(input)?;
+    let (input, _) = line_ending(input)?;
+    let mut input = input;
+    let mut states = vec![];
+    loop {
+        let (loop_input, state) = not_line_ending(input)?;
+        if state == "end_state" {
+            break;
+        }
+        let (_, state) = i32(state)?;
+        states.push(state);
+        let (loop_input, _) = line_ending(loop_input)?;
+        input = loop_input;
+    }
+    Ok((input, states))
 }
 
 
@@ -101,6 +162,13 @@ fn parse_sas_output(input: &str) -> IResult<&str, SasOutput> {
     let variables = vec![];
     let (input, var) = parse_all_variables(input)?;
     println!("Parsed variables: {:?}", var);
+
+    let (input, mutexes) = parse_mutexes(input)?;
+    let (input, states) = parse_state(input)?;
+    println!("Parsed states: {:?}", states);
+
+
+    
 
 
 
