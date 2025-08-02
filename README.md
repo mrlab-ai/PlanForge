f3591eb5804df11645db9e2da6664e0169d15c71
Markus <fritzsma@gmail.com>
2025-08-02 19:05:59 +0200

    Add initial README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Work in Progress Numeric Fast Downward Replacement in Rust
+
+Right now, only a parser, applicable for regular SAS+ files exist. 
+
+## Usage
+```
+cargo run <sas file>
+```
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ mod parser;
 mod search; 
 
 use std::fs;
+use std::env;
 use parser::parse_sas_output;
 
 fn main() {
-    let content = fs::read_to_string("output.sas").expect("Could not read file");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} [sas_file]", args[0]);
+        return;
+    }
+    let sas_file = &args[1];
+    let content = fs::read_to_string(sas_file).expect("Could not read file");
     match parse_sas_output(&content) {
         Ok((_, sas_output)) => {println!("Successfully parsed SAS file")},
         Err(e) => println!("Failed to parse file: {:?}", e),
