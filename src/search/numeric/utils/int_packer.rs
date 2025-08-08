71662bc9ebd28fb2936dba1dec34b4afbd93e3bf
Markus Fritzsche <fritzsma@gmail.com>
2025-08-08 11:15:23 +0200

    Add test to pack double values

--- src/search/numeric/utils/int_packer.rs
+++ src/search/numeric/utils/int_packer.rs
@@ -162,8 +162,8 @@ impl IntDoublePacker {
         f64::from_bits(packed_double)
     }
 
-    pub fn get_double(&self, buffer: &[u64], var: i32, var_infos: Vec<VariableInfo>) -> f64 {
-        let packed_double = var_infos[var as usize].get(buffer);
+    pub fn get_double(&self, buffer: &[u64], var: i32) -> f64 {
+        let packed_double = self.var_infos[var as usize].get(buffer);
         self.unpack_double(packed_double)
     }
 
@@ -181,7 +181,7 @@ mod tests {
     use super::*;
 
     fn setup() -> IntDoublePacker {
-        let ranges = vec![100, 200, 300, 400, 500];
+        let ranges = vec![100, 200, 300, 400, 500, u64::MAX];
         dbg!(&ranges);
         IntDoublePacker::new(ranges)
     }
@@ -189,7 +189,7 @@ mod tests {
     #[test]
     fn pack_and_unpack_ints() {
         let mut packer = setup();
-        let buffer = &mut [0u64; 5];
+        let buffer = &mut [0u64; 6];
 
         // Pack some integers
         packer.set(buffer, 0, 42);
@@ -200,5 +200,21 @@ mod tests {
         assert_eq!(packer.get(buffer, 0), 42);
         assert_eq!(packer.get(buffer, 1), 84);
         assert_eq!(packer.get(buffer, 2), 126);
+    }
+
+    #[test]
+    fn pack_and_unpack_doubles() {
+        let mut packer = setup();
+        let buffer = &mut [0u64; 6];
+
+        let double_var_id = 5;
+
+        let double_value = 3.14;
+        let packed = packer.pack_double(double_value);
+
+        packer.set(buffer, double_var_id, packed);
+
+        let unpacked = packer.get_double(buffer, double_var_id);
+        assert_eq!(unpacked, double_value);
     }
 }
