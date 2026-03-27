78e30193b0d24583cf443c0bd49d8798d50fe857
Markus Fritzsche <fritzsma@gmail.com>
2026-03-27 09:47:09 +0100

    refactor(DA): Avoid printing state distance table

--- planners-search/src/numeric/evaluation/domain_abstractions/domain_abstraction_factory.rs
+++ planners-search/src/numeric/evaluation/domain_abstractions/domain_abstraction_factory.rs
@@ -436,7 +436,7 @@ impl DomainAbstractionFactory {
             return;
         }
         // NOTE: Enable later!
-        //return;
+        return;
 
         // Identify propositional variables derived by propositional axioms.
         let mut is_axiom_var: Vec<bool> = vec![false; num_prop_vars];
